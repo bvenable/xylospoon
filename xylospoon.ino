32aca90ac6cfeb62ceb4396c5de665d585874e8b
Bryan Venable <github@spif.com>
2016-07-27 01:43:53 -0500

    multiple spoons

--- xylospoon.ino
+++ xylospoon.ino
@@ -1,14 +1,29 @@
 void setup()
 {
   pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+}
+
+void pintonote(int pin)
+{
+  int note;
+  note = 58 + pin;
+  if (digitalRead(pin)) {
+    usbMIDI.sendNoteOff(note, 50, 144);
+  } else {
+    usbMIDI.sendNoteOn(note, 50, 144);
+  }
 }
 
 void loop()
 {
-  if (digitalRead(2)) {
-    usbMIDI.sendNoteOff(60, 50, 144);
-  } else {
-    usbMIDI.sendNoteOn(60, 50, 144);
+  int p;
+  for (p = 2; p < 8; p++){
+    pintonote(p);
   }
   delay(10);  
 }
